3d5fc6700de3efd8340a2e7094de698b803f67ee
foxblock <foxblock+github@gmail.com>
2023-11-16 22:21:00 +0100

    Add transition images
    Refactor simon code to use state variable
    Fix some timer related bugs in simon mode
    Fix timer bar not drawing properly in simon mode

--- Source/main.lua
+++ Source/main.lua
@@ -19,7 +19,7 @@
 -- [X] multiply by speed factor
 -- [X] lose on lock
 -- [ ] game mode: last one cranking (versus, each player gets a few actions in sequence)
--- [ ] game mode: simon cranks (solo, you get a sequence and have to do it afterwards)
+-- [X] game mode: simon cranks (solo, you get a sequence and have to do it afterwards)
 -- [ ] game mode: crank the bomb (party, you do an action as fast as possible then pass, bomb explodes after random time)
 -- [X] background animations for actions
 -- [X] sound for actions
@@ -32,6 +32,7 @@
 -- [X] main menu - do not start the game immediately
 -- [X] add title card recommending to play without the cover (https://devforum.play.date/t/crank-docking-not-registered/10439)
 -- [ ] sound convert script: add option to convert single file if passed path is a file
+-- [ ] go to main menu option in menu
 
 
 import "CoreLibs/object"
@@ -601,15 +602,28 @@ local SIMON_TIMER_DURATION_MS <const> = 6500
 local SIMON_TIMER_SHOW_MS <const> = 4300
 local SIMON_TIMER_SOUND2_MS <const> = 2400
 local SIMON_TIMER_SOUND3_MS <const> = 960
+local SIMON_TRANSITION_FRAME_MS <const> = 1000
+
+local SIMON_STATE <const> = {
+    SCORE_UP = 1,
+    WAIT_FOR_UNDOCK = 2,
+    INSTRUCTIONS = 3,
+    SHOW = 4,
+    ACTION = 5
+}
 
 local actionChain = {}
 local score_simon = 0
 local currIndex = 1
-local simonsTurn = true
-local simonWaitForUndock = false
 local simonTimer
-local simonYourTurnImg = gfx.image.new("images/simon")
+
+local simonYourTurnImg = gfx.image.new("images/simon_action")
 local simonDockImg = gfx.image.new("images/simon_dock")
+local simonScoreImg = gfx.image.new("images/simon_score")
+local simonSimonsTurnImg = gfx.image.new("images/simon_show")
+local simonState
+local simonTransitionTimer
+
 local simonTickSlow = playdate.sound.sample.new("sounds/tick1")
 local simonTickMid = playdate.sound.sample.new("sounds/tick2")
 local simonTickFast = playdate.sound.sample.new("sounds/tick3")
@@ -622,15 +636,16 @@ local update_simon_action
 local buttonHandlers_simonDockContinue = {
     crankUndocked = function()
         playdate.inputHandlers.pop()
-        simonWaitForUndock = false
-        setupActionGfxAndSound(currAction, true)
+        simonState = SIMON_STATE.INSTRUCTIONS
+        simonTransitionTimer:reset()
+        simonTransitionTimer:start()
+        playdate.graphics.sprite.redrawBackground()
     end
 }
 
 local function startGame_simon()
     score_simon = 0
     currIndex = 1
-    simonsTurn = true
 
     actionChain = {}
     -- do not allow dock action in this set, so we don't have to track dock state
@@ -639,13 +654,15 @@ local function startGame_simon()
     end
     currAction = actionChain[1]
     if (playdate.isCrankDocked()) then
-        simonWaitForUndock = true
-        playdate.inputHandlers.push(buttonHandlers_simonDockContinue)
+        simonState = SIMON_STATE.WAIT_FOR_UNDOCK
+        playdate.inputHandlers.push(buttonHandlers_simonDockContinue, true)
         playdate.graphics.sprite.redrawBackground()
     else
-        setupActionGfxAndSound(currAction, true)
+        simonState = SIMON_STATE.INSTRUCTIONS
+        playdate.graphics.sprite.redrawBackground()
+        simonTransitionTimer:reset()
+        simonTransitionTimer:start()
     end
-
     currMusic:stop()
 end
 
@@ -658,7 +675,7 @@ local buttonHandlers_simonLose = {
 }
 
 local function actionSuccess_simon()
-    if (simonsTurn) then return end
+    if (simonState ~= SIMON_STATE.ACTION) then return end
 
     soundSuccess:play(1)
     simonSampleplayer:stop()
@@ -674,17 +691,13 @@ local function actionSuccess_simon()
         score_simon += 1
         simonTimer:pause()
         table.insert(actionChain, getValidActionCode(false))
-        simonsTurn = true
         updateFnc = update_simon_show
         currIndex = 1
         currAction = actionChain[1]
-        if (playdate.isCrankDocked()) then
-            simonWaitForUndock = true
-            playdate.inputHandlers.push(buttonHandlers_simonDockContinue)
-            playdate.graphics.sprite.redrawBackground()
-        else
-            setupActionGfxAndSound(currAction, true)
-        end
+        simonState = SIMON_STATE.SCORE_UP
+        simonTransitionTimer:reset()
+        simonTransitionTimer:start()
+        playdate.graphics.sprite.redrawBackground()
     end
 end
 
@@ -704,37 +717,60 @@ local function actionFail_simon()
     currMusic:stop()
     currMusic:setSample(loseMusic)
     currMusic:play(0)
-    playdate.inputHandlers.push(buttonHandlers_simonLose)
+    simonTimer:pause()
+    playdate.inputHandlers.push(buttonHandlers_simonLose, true)
     updateFnc = update_none
+end
+
+local function simonTransitionEnd()
+    if (simonState == SIMON_STATE.SCORE_UP) then
+        if (playdate.isCrankDocked()) then
+            simonState = SIMON_STATE.WAIT_FOR_UNDOCK
+            playdate.inputHandlers.push(buttonHandlers_simonDockContinue, true)
+            playdate.graphics.sprite.redrawBackground()
+        else
+            simonState = SIMON_STATE.INSTRUCTIONS
+            playdate.graphics.sprite.redrawBackground()
+            simonTransitionTimer:reset()
+            simonTransitionTimer:start()
+        end
+    elseif (simonState == SIMON_STATE.INSTRUCTIONS) then
+        simonState = SIMON_STATE.SHOW
+        setupActionGfxAndSound(currAction, true)
+    end
 end
 
 local function render_simon()
     gfx.sprite.update()
 
-    gfx.drawText('score: '..score_simon, 170, 224)
+    if (simonState == SIMON_STATE.ACTION) then
+        if (simonTimer.timeLeft <= SIMON_TIMER_SHOW_MS) then
+            local w = screen.getWidth() * simonTimer.timeLeft / SIMON_TIMER_SHOW_MS
+            gfx.fillRect(0, screen.getHeight() - 20, w, 20)
+        end
 
-    if (simonTimer.timeLeft <= SIMON_TIMER_SHOW_MS) then
-        local w = screen.getWidth() * simonTimer.timeLeft / SIMON_TIMER_SHOW_MS
-        gfx.fillRect(0, screen.getHeight() - 20, w, 20)
-    end
+        gfx.drawText('score: '..score_simon, 170, 224)
 
-    if (saveData.debugOn and not simonsTurn) then
-        local yPos = 2
-        if (currAction == ACTION_CODES.MICROPHONE) then
-            gfx.drawText(string.format("level: %.0f", mic.getLevel() * 100), 2, yPos)
-            yPos += 25
-        elseif (currAction == ACTION_CODES.TILT) then
-            gfx.drawText(string.format("val: %.2f %.2f %.2f", playdate.readAccelerometer()), 2, yPos);
-            gfx.drawText(string.format("a3d: %.2f", math.acos(vec3D_dot(startVec, playdate.readAccelerometer())) * RAD_TO_DEG), 2, yPos + 15)
-            gfx.drawText(string.format("cos: %.4f", vec3D_dot(startVec, playdate.readAccelerometer())), 2, yPos + 30)
-            gfx.drawText(string.format("target: %.4f", TILT_TARGET), 2, yPos + 45)
-            yPos += 70
+        if (saveData.debugOn) then
+            local yPos = 2
+            if (currAction == ACTION_CODES.MICROPHONE) then
+                gfx.drawText(string.format("level: %.0f", mic.getLevel() * 100), 2, yPos)
+                yPos += 25
+            elseif (currAction == ACTION_CODES.TILT) then
+                gfx.drawText(string.format("val: %.2f %.2f %.2f", playdate.readAccelerometer()), 2, yPos);
+                gfx.drawText(string.format("a3d: %.2f", math.acos(vec3D_dot(startVec, playdate.readAccelerometer())) * RAD_TO_DEG), 2, yPos + 15)
+                gfx.drawText(string.format("cos: %.4f", vec3D_dot(startVec, playdate.readAccelerometer())), 2, yPos + 30)
+                gfx.drawText(string.format("target: %.4f", TILT_TARGET), 2, yPos + 45)
+                yPos += 70
+            end
         end
     end
 end
 
 update_simon_show = function ()
-    if (simonWaitForUndock) then goto render end
+    playdate.timer.updateTimers()
+
+    if (simonState ~= SIMON_STATE.SHOW) then goto render end
 
     if (actions[currAction].snd:isPlaying()) then goto render end
 
@@ -743,7 +779,7 @@ update_simon_show = function ()
         currAction = actionChain[currIndex]
         setupActionGfxAndSound(currAction, true)
     else
-        simonsTurn = false
+        simonState = SIMON_STATE.ACTION
         updateFnc = update_simon_action
         currIndex = 1
         currAction = actionChain[1]
@@ -792,16 +828,21 @@ end
 local function setup_simon()
     gfx.sprite.setBackgroundDrawingCallback(
         function( x, y, width, height )
-            if (simonWaitForUndock) then
-                simonDockImg:draw(0,0)
-            elseif (simonsTurn or currAction == ACTION_CODES.LOSE) then
+            if (simonState == SIMON_STATE.SHOW or currAction == ACTION_CODES.LOSE) then
                 actions[currAction].img:draw(0,0)
-            else
+            elseif (simonState == SIMON_STATE.SCORE_UP) then
+                simonScoreImg:draw(0,0)
+            elseif (simonState == SIMON_STATE.WAIT_FOR_UNDOCK) then
+                simonDockImg:draw(0,0)
+            elseif (simonState == SIMON_STATE.INSTRUCTIONS) then
+                simonSimonsTurnImg:draw(0,0)
+            elseif (simonState == SIMON_STATE.ACTION) then
                 simonYourTurnImg:draw(0,0)
             end
         end
     )
 
+    gfx.setColor(gfx.kColorBlack)
     playdate.startAccelerometer()
     playdate.inputHandlers.push(buttonHandlers_main)
     updateFnc = update_simon_show
@@ -811,6 +852,9 @@ local function setup_simon()
     simonTimer = playdate.timer.new(SIMON_TIMER_DURATION_MS, actionTimerEnd)
     simonTimer.discardOnCompletion = false
     simonTimer:pause()
+    simonTransitionTimer = playdate.timer.new(SIMON_TRANSITION_FRAME_MS, simonTransitionEnd)
+    simonTransitionTimer.discardOnCompletion = false;
+    simonTransitionTimer:pause()
 
     startGame_simon()
 end
@@ -910,7 +954,7 @@ local buttonHandlers_title = {
                 setup_main()
             elseif (selectedGame == GAME_MODE.SIMON) then
                 setup_simon()
-            end 
+            end
         end
     end
 }
